f421b77cdb33d36c0c0b7938dcc94d0717ec1438
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-19 02:37:05 -0500

    instance of Menu in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import SearchBar from './components/SearchBar.jsx'; 
+import SearchBar from './components/SearchBar.jsx';
+import NavBar from './assets/component/jsx/NavBar.jsx';
 import './App.css'; 
 
 const App = () => {
   return (
+
     <div className="App">
-      <header>
-        <h1>Busca tu vuelo</h1>
-      </header>
-      {/* Inserta el componente de búsqueda aquí */}
-      <SearchBar />
+      <div className="NavBar">
+        <NavBar />
+      </div>
+
+      <div className="centrado">
+        <header>
+          <h1>Busca tu vuelo</h1>
+        </header>
+        {/* Inserta el componente de búsqueda aquí */}
+        <SearchBar />
+      </div>
+      
+      
+      
     </div>
   );
 };
