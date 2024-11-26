86ef04af4a3d94c5a7c9ee72068902036a6b69c1
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-26 00:54:30 -0500

    mmme

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
       <Routes>
         {/* Rutas con Layout */}
         <Route
-          path="/ListAirlines "
+          path="/api/v1/ListAirlines "
           element={
             <Layout>
               <ListAirlines />
